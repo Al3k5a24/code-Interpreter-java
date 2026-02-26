cea7f6b1b86e95298e7259dce08174854408ce88
Al3k5a24 <leki.djekic@gmail.com>
2026-02-26 02:11:08 +0100

    Added keywords hashmap and modified identifier function with added hashmap usage

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Scanner {
     private final String source;
@@ -36,9 +38,7 @@ public class Scanner {
 
     // za tokene koji imaju literal vrednost(mora da se doradi)
     private void addToken(TokenType type, Object literal){
-
         String text = source.substring(start, current);
-
         tokens.add(new Token(type,text,literal,line));
     }
 
@@ -158,20 +158,46 @@ private void number(){
         return source.charAt(current + 1);
     }
 
-
     private void identifier() {
         while (isAlphaNumeric(peek())) advance();
-        addToken(IDENTIFIER);
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = TokenType.IDENTIFIER;
+        addToken(type);
+
+        addToken(TokenType.IDENTIFIER);
     }
 
     private boolean isAlpha(char c) {
         return (c >= 'a' && c <= 'z') ||
                 (c >= 'A' && c <= 'Z') ||
-                c == '_';s
+                c == '_';
     }
 
     private boolean isAlphaNumeric(char c) {
         return isAlpha(c) || isDigit(c);
+    }
+
+    private static final Map<String, TokenType> keywords;
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    TokenType.AND);
+        keywords.put("class",  TokenType.CLASS);
+        keywords.put("else",   TokenType.ELSE);
+        keywords.put("false",  TokenType.FALSE);
+        keywords.put("for",    TokenType.FOR);
+        keywords.put("fun",    TokenType.FUN);
+        keywords.put("if",     TokenType.IF);
+        keywords.put("nil",    TokenType.NIL);
+        keywords.put("or",     TokenType.OR);
+        keywords.put("print",  TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super",  TokenType.SUPER);
+        keywords.put("this",   TokenType.THIS);
+        keywords.put("true",   TokenType.TRUE);
+        keywords.put("var",    TokenType.VAR);
+        keywords.put("while",  TokenType.WHILE);
     }
 }
 
