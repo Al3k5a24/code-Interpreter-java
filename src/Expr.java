6f8fb93f85bd1ce90fc184ab5859b35a4ae3e83b
Al3k5a24 <leki.djekic@gmail.com>
2026-02-27 01:01:17 +0100

    Starting AST classes implementation

---
+++ src/Expr.java
@@ -0,0 +1,15 @@
+public abstract class Expr {
+
+    //klasa za binarne izraze
+    static class Binary extends Expr {
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+
+        final Expr left;
+        final Token operator;
+        final Expr right;
+    }
+}
